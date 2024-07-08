412d0316e9f2b9b8ed7608e6d2007fa9c4c063b3
Sheryl0823 <sobrevillasherylpraxedes@gmail.com>
2024-07-08 08:41:15 +0800

    Update insert.php

--- api/insert.php
+++ api/insert.php
@@ -1,33 +1,24 @@
 <?php
-    $CN = mysqli_connect("localhost", "root", "", "account");
-
-    // Check connection
-    if (mysqli_connect_errno()) {
-        echo "Failed to connect to MySQL: " . mysqli_connect_error();
-        exit();
-    }
+    $CN = mysqli_connect("localhost", "root", "");
+    $DB = mysqli_select_db($CN, "account");
 
     $EncodedData = file_get_contents('php://input');
     $DecodedData = json_decode($EncodedData, true);
 
-    $email = mysqli_real_escape_string($CN, $DecodedData['email']);
-    $fullname = mysqli_real_escape_string($CN, $DecodedData['fullname']);
-    $age = mysqli_real_escape_string($CN, $DecodedData['age']);
-    $password = mysqli_real_escape_string($CN, $DecodedData['password']);
-    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+    $email = $DecodedData['email'];
+    $fullname = $DecodedData['fullname'];
+    $age = $DecodedData['age'];
+    $password = md5($DecodedData['password']);
 
-    $insertMemberData = "INSERT INTO users (email, fullname, age, password) 
-                         VALUES ('$email', '$fullname', '$age', '$hashed_password')";
+    $insertMemberData = "insert into users(email, fullname, age, password) values ('$email', '$fullname', '$age', '$password')";
 
-    if (mysqli_query($CN, $insertMemberData)) {
-        $Message = "Successfully Registered!!!";
-    } else {
-        $Message = "Server Error... please try later";
-    }
+    $register = mysqli_query($CN, $insertMemberData);
+
+    if ($register) 
+        $Message = "Member has been registered successfully";
+    else 
+        $Message = "Server Error... please try latter";
 
     $Response[] = array("Message" => $Message);
     echo json_encode($Response);
-
-    // Close database connection
-    mysqli_close($CN);
 ?>
